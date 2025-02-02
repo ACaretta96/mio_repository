485e9c41022baa8e108e40e3d79e69e8540c0ae9
alberto_caretta <a.caretta@almaviva.it>
2025-02-02 18:46:25 +0100

    First Commit by Alberto

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,10 +2,21 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.print("Hello and welcom!");
 
-        for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
-        }
+        Contatto pers1=new Contatto("Gianni", "Rossi", 334);
+        Contatto pers2=new Contatto("Filippo", "Bianchi", 335);
+        Contatto pers3=new Contatto("Carlo", "Gialli", 334);
+        Contatto pers4=new Contatto("Lucia", "DeLaCruz", 335);
+        Contatto pers5=new Contatto("Federica", "Rodriguez", 334);
+        Contatto pers6=new Contatto("Valeria", "DeLaVega", 335);
+        Rubrica sim=new Rubrica();
+        sim.aggiungiContatto(pers1);
+        sim.aggiungiContatto(pers2);
+        sim.aggiungiContatto(pers3);
+        sim.aggiungiContatto(pers4);
+        sim.aggiungiContatto(pers5);
+        sim.aggiungiContatto(pers6);
+        sim.visualizzaContatti();
+        System.out.println(sim.cercaContattoPerNome("Giann"));;
     }
 }
